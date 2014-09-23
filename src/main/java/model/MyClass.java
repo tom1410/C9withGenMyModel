66e69ba0a0a2d2819bf6fb1eaa6be37838eec94d
GenMyModel <help@genmymodel.com>
2014-09-23 08:12:55 +0000

    commit by GenMyModel using genmymodel: v20140923_081254

---
+++ src/main/java/model/MyClass.java
@@ -0,0 +1,22 @@
+package model;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class MyClass
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public MyClass(){
+		super();
+	}
+
+}
+
